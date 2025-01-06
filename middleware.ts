80de4f7aa8d71eb7a9a4d3c02eb0e73fc6168d5d
Qvatra <mail2zin@gmail.com>
2025-01-06 12:40:00 +0100

    remove redundant func in middleware

--- middleware.ts
+++ middleware.ts
@@ -1,35 +1,4 @@
-import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
-import { aboutContent } from './content/about';
-
-const SUPPORTED_LANGUAGES = Object.keys(aboutContent);
-const DEFAULT_LANGUAGE = 'en';
-
-function getPreferredLanguage(request: NextRequest): string {
-  // Check cookie first
-  const cookieLang = request.cookies.get('preferredLanguage')?.value;
-  if (cookieLang && SUPPORTED_LANGUAGES.includes(cookieLang)) {
-    return cookieLang;
-  }
-
-  // Check Accept-Language header
-  const acceptLanguage = request.headers.get('accept-language');
-  if (acceptLanguage) {
-    const preferredLanguages = acceptLanguage
-      .split(',')
-      .map(lang => lang.split(';')[0].split('-')[0].toLowerCase());
-
-    const matchedLanguage = preferredLanguages.find(lang => 
-      SUPPORTED_LANGUAGES.includes(lang)
-    );
-    
-    if (matchedLanguage) {
-      return matchedLanguage;
-    }
-  }
-
-  return DEFAULT_LANGUAGE;
-}
 
 export function middleware(request: NextRequest) {
   const pathname = request.nextUrl.pathname;
@@ -37,4 +6,4 @@ export function middleware(request: NextRequest) {
 
 export const config = {
   matcher: ['/'],
-}; 
+};
